e59115f95f594e217f5bec5caefde9a55928df22
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-06-17 11:23:22 +0600

    Updated to interact with token contract

--- contracts/PredictionMarket.sol
+++ contracts/PredictionMarket.sol
@@ -12,38 +12,57 @@ contract PredictionMarket {
     }
     Result result;
     bool electionFinished;
+    address tokenAddress;
 
     mapping(Side => uint256) public bets;
     mapping(address => mapping(Side => uint256)) public betsPerGambler;
     address public oracle;
 
-    constructor(address _oracle) {
+    constructor(address _oracle, address _tokenAddress) {
         oracle = _oracle;
+        tokenAddress = _tokenAddress;
     }
 
-    function placeBet(Side _side) external payable {
-        require(electionFinished == false, "election is finished");
-        bets[_side] += msg.value;
-        betsPerGambler[msg.sender][_side] += msg.value;
+    function setTokenAddress(address _tokenAddress) external {
+        tokenAddress = _tokenAddress;
+    }
+
+    function placeBet(Side _side, uint256 amount) external {
+        // require(electionFinished == false, "election is finished");
+        IESDToken(tokenAddress).approveTx(address(this), amount);
+        bets[_side] += amount;
+        betsPerGambler[msg.sender][_side] += amount;
     }
 
     function withdrawGain() external {
         uint256 gamblerBet = betsPerGambler[msg.sender][result.winner];
         require(gamblerBet > 0, "you do not have any winning bet");
-        require(electionFinished == true, "election not finished yet");
-        address payable winner = payable(msg.sender);
+        // require(electionFinished == true, "election not finished yet");
+        // address payable winner = payable(msg.sender);
         uint256 gain = gamblerBet +
             (bets[result.loser] * gamblerBet) /
             bets[result.winner];
         betsPerGambler[msg.sender][Side.Biden] = 0;
         betsPerGambler[msg.sender][Side.Trump] = 0;
-        winner.transfer(gain);
+        IESDToken(tokenAddress)._transferFrom(address(this), msg.sender, gain);
     }
 
     function reportResult(Side _winner, Side _loser) external {
         require(oracle == msg.sender, "only oracle");
         result.winner = _winner;
         result.loser = _loser;
-        electionFinished = true;
+        // electionFinished = true;
     }
+}
+
+interface IESDToken {
+    function _transferFrom(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) external;
+
+    function approveTx(address _spender, uint256 _amount) external;
+
+    function allow(address _from, address _to) external view;
 }
